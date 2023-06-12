13cd04a65480ebb7dd5e4ade3ea45883699fffa0
Goathoon <ownest2@gmail.com>
2023-06-12 22:23:57 +0900

    chore: 타임리프 템플릿엔진 dependency 추가
    추가 및, templates 디렉토리내에 test.html추가하여 동적 렌더링 테스트

--- src/main/java/Funssion/Inforum/controller/test_controller.java
+++ src/main/java/Funssion/Inforum/controller/test_controller.java
@@ -1,9 +1,13 @@
 package Funssion.Inforum.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class test_controller {
-
+    @GetMapping("test")
+    public String test(Model model){
+        return "test";
+    }
 }
