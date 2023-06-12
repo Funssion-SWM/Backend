22e9386cfa9543518c694465159e6b005554ea8d
Goathoon <ownest2@gmail.com>
2023-06-12 20:51:40 +0900

    Init: Spring Boot 프로젝트 생성
    Dependency는 하나도 추가하지 않음.
    말그대로 초기 설정입니다.

---
+++ src/test/java/Funssion/Inforum/InforumApplicationTests.java
@@ -0,0 +1,13 @@
+package Funssion.Inforum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InforumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
