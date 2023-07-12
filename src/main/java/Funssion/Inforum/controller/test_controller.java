ca3b0b106d0c3f947dc8fac13d66a6a13b139d05
jinu <jinu@jinus-MacBook-Air.local>
2023-07-12 17:08:20 +0900

    feat : implement memo API with swagger

--- src/main/java/Funssion/Inforum/controller/test_controller.java
+++ src/main/java/Funssion/Inforum/controller/test_controller.java
@@ -3,11 +3,14 @@ package Funssion.Inforum.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@ResponseStatus
 public class test_controller {
-    @GetMapping("test")
-    public String test(Model model){
+    @GetMapping("/test")
+    public String test(){
         return "test";
     }
 }
---
+++ src/main/java/Funssion/Inforum/swagger/memo/controller/MemoController.java
@@ -0,0 +1,77 @@
+package Funssion.Inforum.swagger.memo.controller;
+
+import Funssion.Inforum.swagger.memo.entity.MemoEntity;
+import Funssion.Inforum.swagger.memo.form.MemoCreateDataForm;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequestMapping("/memos")
+@Tag(name = "memo", description = "memo API")
+public class MemoController {
+    @Operation(summary = "Get memo list",description = "get memo list in main page and my page", tags = {"memo"})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "successful operation", content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "400", description = "bad request, query is not defined"),
+            @ApiResponse(responseCode = "404", description = "user not found"),
+    })
+    @GetMapping
+    public List<MemoEntity> memos(@Parameter(description = "criteria in hot memos, period={day, week, month, year}, defult is day", required = false) @RequestParam String period,
+                                  @Parameter(description = "criteria in sorting, sortBy={hot, new}, default is hot", required = false) @RequestParam String sortBy,
+                                  @Parameter(description = "get one user's memo list", required = false) @RequestParam String userId) {
+        ArrayList<MemoEntity> memos = new ArrayList<MemoEntity>();
+        memos.add(new MemoEntity(1, 1, "정진우", "JDK란?", "JDK이다.", "green", LocalDate.now(), LocalDate.now()));
+        memos.add(new MemoEntity(2, 2, "김태훈", "DB란?", "H2이다.", "black", LocalDate.now(), LocalDate.now()));
+        return memos;
+    }
+
+    @Operation(summary = "Get memo", tags = {"memo"})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "successful operation", content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "404", description = "Memo not found", content = @Content),
+    })
+    @GetMapping("{id}")
+    public MemoEntity memo(@Parameter(description = "memo id") @PathVariable String id) {
+        return new MemoEntity(1, 1, "정진우", "JDK란?", "JDK이다.", "green", LocalDate.now(), LocalDate.now());
+    }
+
+    @Operation(summary = "Create memo", tags = {"memo"})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "successful created", content = @Content(mediaType = "application/json")),
+    })
+    @PostMapping(consumes = "application/json")
+    public MemoEntity creteMemo(@Parameter(description = "create form") @RequestBody MemoCreateDataForm createDataForm) {
+        return new MemoEntity(1, 1, "정진우", "JDK란?", "JDK이다.", "green", LocalDate.now(), LocalDate.now());
+    }
+
+    @Operation(summary = "Update memo", tags = {"memo"})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "successful updated", content = @Content),
+            @ApiResponse(responseCode = "404", description = "memo not found", content = @Content),
+    })
+    @PostMapping(value = "{id}",consumes = "application/json")
+    public MemoEntity updateMemo(@Parameter(description = "create form") @RequestBody MemoCreateDataForm createDataForm,
+                           @Parameter(description = "memo id") @PathVariable int id) {
+        return new MemoEntity(1, 1, "정진우", "JDK란?", "JDK이다.", "green", LocalDate.now(), LocalDate.now());
+    }
+
+    @Operation(summary = "Delete memo", tags = {"memo"})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "successful deleted", content = @Content),
+            @ApiResponse(responseCode = "404", description = "memo not found", content = @Content),
+    })
+    @DeleteMapping(value = "{id}",consumes = "application/json")
+    public void deleteMemo(@Parameter(description = "memo id") @PathVariable int id) {}
+}
