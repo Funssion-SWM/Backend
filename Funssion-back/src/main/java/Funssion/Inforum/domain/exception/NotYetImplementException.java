ac2dc080d844ef4292ffe35420d82b739380a293
Goathoon <ownest2@gmail.com>
2023-07-15 16:24:51 +0900

    Revert "chore: move folder"
    This reverts commit b4683783443a4316fe5721b5558d46ce0520b00f.

--- Funssion-back/src/main/java/Funssion/Inforum/domain/exception/NotYetImplementException.java
+++
@@ -1,7 +0,0 @@
-package Funssion.Inforum.domain.exception;
-
-public class NotYetImplementException extends RuntimeException {
-    public NotYetImplementException(String msg){
-        super(msg);
-    }
-}
--- Funssion-back/src/main/java/Funssion/Inforum/domain/member/entity/SocialMember.java
+++
@@ -1,9 +0,0 @@
-package Funssion.Inforum.domain.member.entity;
-
-import lombok.Getter;
-import lombok.Setter;
-
-@Setter @Getter
-public class SocialMember extends Member {
-/* 추가 필요 */
-}
