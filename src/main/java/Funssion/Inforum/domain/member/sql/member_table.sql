dc686511ad586cb9bfb76e8cee2177d112be08f0
Goathoon <ownest2@gmail.com>
2023-07-18 22:25:51 +0900

    fix: fix sql db setting for postgresql

--- src/main/java/Funssion/Inforum/domain/member/sql/member_table.sql
+++ src/main/java/Funssion/Inforum/domain/member/sql/member_table.sql
@@ -1,16 +1,15 @@
-create table Member.user(
-    user_id INT AUTO_INCREMENT,
+create table Member.member_user(
+    user_id SERIAL primary key,
     user_name VARCHAR(15) NOT NULL UNIQUE,
     login_type INT NOT NULL DEFAULT 0,
-    created_date TIMESTAMP,
-    PRIMARY KEY (user_id)
-);
+    created_date TIMESTAMP
+)
 
-create table Member.auth(
-    auth_id INT AUTO_INCREMENT,
+create table Member.member_auth(
+    auth_id SERIAL,
     user_id INT NOT NULL,
     user_email VARCHAR(60) NOT NULL UNIQUE,
     user_pw VARCHAR(100) NOT NULL,
     PRIMARY KEY (auth_id),
-    FOREIGN KEY (user_id) REFERENCES user(user_id)
+    FOREIGN KEY (user_id) REFERENCES member.member_user(user_id)
 );
