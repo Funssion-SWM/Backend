4862ca2179d35431000ffac1a1b5ad16405f492e
comolove <jungjinwoo41@gmail.com>
2023-08-11 18:31:08 +0900

    fix: get like info api response type

---
+++ src/main/java/Funssion/Inforum/domain/like/dto/response/IsLikeDto.java
@@ -0,0 +1,10 @@
+package Funssion.Inforum.domain.like.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class IsLikeDto {
+    private Boolean isLike;
+}
