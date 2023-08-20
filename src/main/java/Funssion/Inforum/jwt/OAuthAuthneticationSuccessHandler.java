bf0cace13f0dc8c66e39e4ca68673574e5d789cf
Goathoon <ownest2@gmail.com>
2023-08-20 23:45:11 +0900

    fix: redirect  URI #4

--- src/main/java/Funssion/Inforum/jwt/OAuthAuthneticationSuccessHandler.java
+++ src/main/java/Funssion/Inforum/jwt/OAuthAuthneticationSuccessHandler.java
@@ -22,7 +22,9 @@ import java.util.Collection;
 public class OAuthAuthneticationSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
     private final TokenProvider tokenProvider;
     @Value("${jwt.domain}") private String domain;
-    @Value("${hostname}") private String host;
+    @Value("${oauth-signup-uri}") private String signUpURI;
+    @Value("${oauth-signin-uri}") private String signInURI;
+
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
                                         Authentication authentication) throws IOException {
@@ -46,14 +48,14 @@ public class OAuthAuthneticationSuccessHandler extends SavedRequestAwareAuthenti
         //사실 authority 가 ROLE_FIRST_JOIN인게 이상하긴함. 하지만 authentication 객체를 활용하기 위해서 해당 방법을 사용하였음.
         //어차피 role은 우리 로직엔 사용되지 않기 때문임.
         if(authorities.stream().filter(o -> o.getAuthority().equals("ROLE_FIRST_JOIN")).findAny().isPresent()){
-            return UriComponentsBuilder.fromHttpUrl("https://"+host+".inforum.me/signup/nickname/")
+            return UriComponentsBuilder.fromHttpUrl(signUpURI)
                     .path(authentication.getName())
                     .build().toString();
 
         }
         else{
-            return UriComponentsBuilder.fromHttpUrl("https://"+host+".inforum.me/memos")
-                .build().toString();
+            return UriComponentsBuilder.fromHttpUrl(signInURI)
+                    .build().toString();
         }
     }
 }
