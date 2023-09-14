7f963bc9f2c35e6cf6adb725bdedcff9a9897626
Goathoon <ownest2@gmail.com>
2023-09-14 17:36:34 +0900

    test: fix null field of MEMO

--- src/test/java/Funssion/Inforum/domain/member/controller/MemberControllerTest.java
+++ src/test/java/Funssion/Inforum/domain/member/controller/MemberControllerTest.java
@@ -9,7 +9,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -44,7 +43,7 @@ class MemberControllerTest {
                 .andReturn();
         String responseBody = result.getResponse().getContentAsString();
         String email = JsonPath.read(responseBody, "$.email");
-        assertThat(email).isEqualTo("test1@gmail.com");
+        assertThat(email).isEqualTo("tes**@gmail.com");
 
     }
 
