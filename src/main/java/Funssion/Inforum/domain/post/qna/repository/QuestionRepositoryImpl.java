4bd31300eb66b0c2656a770deb6c31a4c2fe1f47
Goathoon <ownest2@gmail.com>
2023-09-30 01:24:01 +0900

    test: controller get question 단위테스트 추가:

--- src/main/java/Funssion/Inforum/domain/post/qna/repository/QuestionRepositoryImpl.java
+++ src/main/java/Funssion/Inforum/domain/post/qna/repository/QuestionRepositoryImpl.java
@@ -81,7 +81,7 @@ public class QuestionRepositoryImpl implements QuestionRepository {
         }
     }
     @Override
-    public List<Question> getQuestionsOfMemo(Long memoId) {
+    public List<Question> getQuestionsOfMemo(Long questionId) {
         String sql = "select id, author_id, author_name, author_image_path, title, text, description, likes, is_solved, created_date, updated_date, tags, answers, is_solved, memo_id " +
                 "from question.info order by created_date desc";
         return template.query(sql,questionRowMapper());
