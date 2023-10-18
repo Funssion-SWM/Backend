22c546dc5876aadbe669aaf43154ca6308a1f36c
Goathoon <ownest2@gmail.com>
2023-10-18 14:54:39 +0900

    fix: score controller api endpoint

--- src/main/java/Funssion/Inforum/domain/score/ScoreController.java
+++ src/main/java/Funssion/Inforum/domain/score/ScoreController.java
@@ -3,9 +3,11 @@ package Funssion.Inforum.domain.score;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/score")
+@RestController
+@RequestMapping("/score")
 @RequiredArgsConstructor
 public class ScoreController {
     private final ScoreService scoreService;
