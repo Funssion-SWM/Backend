ff710e2272440cb7822abe2ac6bdff6d966d33a7
Goathoon <ownest2@gmail.com>
2023-10-19 19:13:26 +0900

    fix: 댓글이 3개이상일때 삭제하는경우 발생하는 exception 수정

--- src/main/java/Funssion/Inforum/domain/post/comment/repository/CommentRepositoryImpl.java
+++ src/main/java/Funssion/Inforum/domain/post/comment/repository/CommentRepositoryImpl.java
@@ -92,16 +92,13 @@ public class CommentRepositoryImpl implements CommentRepository{
         String sql = "select post_id, post_type from post.comment where id = ?";
         return template.queryForObject(sql,postIdAndPostTypeRowMapper(),commentId);
     }
-    public Optional<Comment> findIfUserRegisterAnotherCommentOfPost(Long userId, Long commentId){
+    public List<Comment> findIfUserRegisterAnotherCommentOfPost(Long userId, Long commentId){
         PostInfo postInfoOfComment = getPostInfoOfComment(userId, commentId);
 
         String sql = "select id,post_id, author_id,author_image_path, author_name, author_rank, likes, recomments, post_type, comment_text, created_date, updated_date" +
                 " from post.comment where author_id = ? and post_type = ? and post_id = ? and id != ?";
-        try {
-            return Optional.ofNullable(template.queryForObject(sql, commentRowMapper(), userId,postInfoOfComment.getPostType().toString(),postInfoOfComment.getPostId(),commentId));
-        }catch(EmptyResultDataAccessException e){
-            return Optional.empty();
-        }
+
+        return template.query(sql, commentRowMapper(), userId,postInfoOfComment.getPostType().toString(),postInfoOfComment.getPostId(),commentId);
     }
     private PostInfo getPostInfoOfComment(Long userId, Long postId){
         String sql = "select post_id, post_type" +
--- src/test/java/Funssion/Inforum/domain/score/ScoreIntegrationTest.java
+++ src/test/java/Funssion/Inforum/domain/score/ScoreIntegrationTest.java
@@ -254,8 +254,9 @@ class ScoreIntegrationTest {
             assertThat(scoreRepository.findScoreHistoryInfoById(sameUserPostCommentOfSameMemo2.getAuthorId(), ScoreType.MAKE_COMMENT, sameUserPostCommentOfSameMemo2.getId()).isPresent()).isEqualTo(true);
             assertThat(scoreRepository.getUserDailyScore(sameUserPostCommentOfSameMemo1.getAuthorId())).isEqualTo(ScoreType.MAKE_COMMENT.getScore() * 2);
         }
+
         @Test
-        @DisplayName("같은 글에 댓글을 2개이상 등록하고, 최초 등록 댓글을 삭제할 경우, 나머지 댓글로 점수를 다시 반영한다.")
+        @DisplayName("같은 글에 댓글을 3개이상 등록하고, 최초 등록 댓글을 삭제할 경우, 나머지 댓글로 점수를 다시 반영한다.")
         void onlyOneCommentOfPostApplyScoreWhenDeleteFirstComment(){
             MemoDto memoDto = createMemo();//saveMemberIdForEachTest 유저가 메모작성
             Comment sameUserPostCommentOfSameMemo1 = commentService.createComment(CommentSaveDto.builder()
@@ -264,6 +265,11 @@ class ScoreIntegrationTest {
                     .commentText("{\"type\": \"doc\", \"content\": [{\"type\": \"paragraph\", \"content\": [{\"text\": \"메모 댓글 내용\", \"type\": \"text\"}]}]}")
                     .build(), saveMemberId);
             Comment sameUserPostCommentOfSameMemo2 = commentService.createComment(CommentSaveDto.builder()
+                    .postId(memoDto.getMemoId())
+                    .postTypeWithComment(PostType.MEMO)
+                    .commentText("{\"type\": \"doc\", \"content\": [{\"type\": \"paragraph\", \"content\": [{\"text\": \"메모 댓글 내용\", \"type\": \"text\"}]}]}")
+                    .build(), saveMemberId);
+            Comment sameUserPostCommentOfSameMemo3 = commentService.createComment(CommentSaveDto.builder()
                     .postId(memoDto.getMemoId())
                     .postTypeWithComment(PostType.MEMO)
                     .commentText("{\"type\": \"doc\", \"content\": [{\"type\": \"paragraph\", \"content\": [{\"text\": \"메모 댓글 내용\", \"type\": \"text\"}]}]}")
