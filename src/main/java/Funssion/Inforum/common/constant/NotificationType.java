b1902038339185c3a9c5a314ae8752e9210b609d
comolove <jungjinwoo41@gmail.com>
2023-10-20 13:43:37 +0900

    chore: modify notification message

--- src/main/java/Funssion/Inforum/common/constant/NotificationType.java
+++ src/main/java/Funssion/Inforum/common/constant/NotificationType.java
@@ -11,7 +11,7 @@ public enum NotificationType {
     NEW_ANSWER("질문에 대한 새로운 답변이 있습니다."),
     NEW_QUESTION("메모에 대한 새로운 질문이 있습니다."),
     NEW_FOLLOWER("새로운 유저가 나를 팔로우했습니다."),
-    NEW_POST_FOLLOWED("팔로잉 중인 사용자가 새로운 컨텐츠를 업로드했습니다."),
+    NEW_POST_FOLLOWED("팔로잉한 유저가 새 컨텐츠를 업로드했습니다."),
     NEW_ACCEPTED("답변이 채택되었습니다.");
 
     public static NotificationType of(String string) {
