f515fab702999d82c997a477cb5fe8c4c386b04b
Goathoon <ownest2@gmail.com>
2023-11-14 16:22:20 +0900

    Revert "fix: 일반로그인 계정으로 구글로그인시 redirect"
    This reverts commit e8178e55984b5d4e2b39815f537a6a1cea949479.

--- src/main/java/Funssion/Inforum/access_handler/OAuthAuthenticationFailureHandler.java
+++ src/main/java/Funssion/Inforum/access_handler/OAuthAuthenticationFailureHandler.java
@@ -6,22 +6,16 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
-import org.springframework.web.util.UriComponentsBuilder;
 
 import java.io.IOException;
 @Component
 public class OAuthAuthenticationFailureHandler implements AuthenticationFailureHandler {
-    @Value("${oauth-signin-uri}") private String signInURI;
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
-        response.sendRedirect(UriComponentsBuilder.fromHttpUrl(signInURI)
-                .build().toString());
         makeFailureResponseBody(response);
-
     }
     private void makeFailureResponseBody(HttpServletResponse response) throws IOException {
         String failureResponse = convertFailureObjectToString();
